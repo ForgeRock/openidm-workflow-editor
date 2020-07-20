fcd5403b5ce073872925462e7e053be36e534361
Adam Crockett <adam.crockett@forgerock.com>
2020-07-20 14:14:30 +0100

    bugfix/IAM-535 license headers added to all sources.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright (c) 2020 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
 import Vue from 'vue';
 import { ToastPlugin, ModalPlugin, TooltipPlugin } from 'bootstrap-vue';
 import axios from 'axios';
--- src/views/Editor/index.vue
+++ src/views/Editor/index.vue
@@ -1,3 +1,10 @@
+<!--
+Copyright (c) 2020 ForgeRock. All rights reserved.
+
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details.
+-->
+
 <template>
   <div class="flex-column d-flex h-100 w-100">
     <BNavbar
